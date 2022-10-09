e7a175734a40879e5b70fd99da34fad2d6e6dc9e
Ben Osborne <osborne.ben@gmail.com>
2022-10-09 12:10:45 -0500

    Renamed project

--- src/AspNetExtensions/WebApplicationBuilderExtensions.cs
+++ src/AspNetExtensions/WebApplicationBuilderExtensions.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 
-namespace OsborneSupremacy2.Extensions.AspNet;
+namespace OsborneSupremacy.Extensions.AspNet;
 
 public static class WebApplicationBuilderExtensions
 {
