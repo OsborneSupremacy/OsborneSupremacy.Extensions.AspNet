79cc5a691de10810e1dfcc5c7844e1debde9cb8b
Ben Osborne <osborne.ben@gmail.com>
2023-02-27 21:50:34 -0600

    Created value-less Outcome without generic type.

--- src/AspNetExtensions/Outcome.cs
+++ src/AspNetExtensions/Outcome.cs
@@ -68,3 +68,38 @@ public record Outcome<T>
 
     public static implicit operator Outcome<T>(T value) => new(value);
 }
+
+public record Outcome
+{
+    public bool IsSuccess { get; }
+
+    public bool IsFaulted => !IsSuccess;
+
+    /// <exception cref="InvalidOperationException">Thrown if this property is accessed when <see cref="IsFaulted"> is not true.</see></exception>
+    public Exception Exception =>
+        IsSuccess ? throw new InvalidOperationException("Cannot access exception when successful.") : _exception!;
+
+    private readonly Exception? _exception;
+
+    public Outcome()
+    {
+        IsSuccess = true;
+    }
+
+    public Outcome(Exception exception)
+    {
+        _exception = exception;
+        IsSuccess = false;
+    }
+
+    public IEnumerable<ValidationFailure> GetValidationErrors()
+    {
+        if (IsSuccess)
+            throw new InvalidOperationException("Cannot access validation exceptions when successful.");
+
+        if (_exception is not ValidationException validationException)
+            return Enumerable.Empty<ValidationFailure>();
+
+        return validationException.Errors;
+    }
+}
